4a82b301765d86dcc214e5fafa19e626c1fe5394
Jonathan Freeman <freethejazz@gmail.com>
2013-04-27 22:34:52 -0500

    Modifying collection name

--- dataclean.js
+++ dataclean.js
@@ -1,8 +1,8 @@
 var conn = new Mongo();
 var db = conn.getDB("rackdata");
 
-db.chicago.find().forEach(function(data){
-    db.chicago.update({_id:data._id}, {$set: {loc: [data.Longitude, data.Latitude]}});
+db.racks.find().forEach(function(data){
+    db.racks.update({_id:data._id}, {$set: {loc: [data.Longitude, data.Latitude], city: "chicago"}});
 });
 
-db.chicago.ensureIndex({"loc":"2d"});
+db.racks.ensureIndex({"loc":"2d"});
--- dbinit.sh
+++ dbinit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-mongoimport --db rackdata --collection chicago --type csv --headerline --file osd-bike-racks/data/Bike_Racks.csv
+mongoimport --db rackdata --collection racks --type csv --headerline --file osd-bike-racks/data/Bike_Racks.csv
 
 mongo dataclean.js
